8083d5d0c05f2c1ed4f35d628cd17652a65ced06
Aleksandr Tumanov <tumalexx02@gmail.com>
2024-06-16 21:17:48 +0500

    Added about section
    Добавлен заголовок секции "о компании", написан первоначальный скрипт для переключения между историей и вакансиями

--- js/script.js
+++ js/script.js
@@ -7,3 +7,20 @@ window.addEventListener('scroll', () => {
     header.classList.remove('lapik-header__painted');
   }
 });
+
+
+
+const aboutSections = document.querySelector('.lapik-about')
+const aboutButtons = document.querySelectorAll('.lapik-about__button');
+
+aboutButtons.forEach((btn, _, btns) => btn.addEventListener('click', (e) => {
+  if (e.target.classList.contains('lapik-about__button_active')) {
+    e.target.classList.remove('lapik-about__button_active');
+    aboutSections.classList.remove('lapik-about_active')
+    return;
+  };
+
+  btns.forEach(button => button.classList.remove('lapik-about__button_active'))
+  e.target.classList.add('lapik-about__button_active');
+  aboutSections.classList.add('lapik-about_active');
+}));
