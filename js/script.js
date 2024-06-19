7b65ee310e0e03858f28760ed9f8aa151039ac0c
Aleksandr Tumanov <tumalexx02@gmail.com>
2024-06-19 15:20:56 +0500

    Adoptation... againg
    Продолжаю адаптировать блоки

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const header = document.querySelector('.lapik-header');
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 100) {
+  if (window.scrollY > 40) {
     header.classList.add('lapik-header__painted');
   } else {
     header.classList.remove('lapik-header__painted');
